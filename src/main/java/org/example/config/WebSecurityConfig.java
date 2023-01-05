c7c5ea18824c6027170ede4bcb3548e8bdb1473b
Kharchenko Volodymyr <kharchenko_ak19@nuwm.edu.ua>
2023-01-05 16:01:42 +0200

    fixed afterlogin path

--- src/main/java/org/example/config/WebSecurityConfig.java
+++ src/main/java/org/example/config/WebSecurityConfig.java
@@ -51,7 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").authenticated()
+                .antMatchers("hotel/all").authenticated()
                 .antMatchers("/admin/**").hasAuthority("ADMIN")
                 .anyRequest().permitAll()
                 .and()
